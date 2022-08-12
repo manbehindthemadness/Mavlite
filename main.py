61bf5cd86418e4b5ce8b100b3c519eba5957643c
CasperClarke <CameronClarke@verizon.net>
2022-08-12 09:13:00 -0500

    First Commit
    Currently Experiencing memory problems with the import of the dialect file.

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Example of how to connect pymavlink to an autopilot via an UDP connection
+"""
+
+# Disable "Bare exception" warning
+# pylint: disable=W0702
+
+import time
+# Import mavutil
+from micropymavlink import mavutil
+
+#mavutil.set_dialect("mavgen_python_dialect")
+
+master = mavutil.mavlink_connection("COM11", baud=57600)
+
+# Make sure the connection is valid
+master.wait_heartbeat()
+
+
+print("Loaded")
+# Get some information !
+while True:
+    try:
+        print(master.recv_match().to_dict())
+    except:
+        pass
+    time.sleep(0.1)
