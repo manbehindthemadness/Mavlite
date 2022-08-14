775ee3f0d1f9eddc016e8c42c3f8c626c48be762
Kevin Eales <manbehindthemadness@gmail.com>
2022-08-14 14:51:54 -0400

    Revised example.

--- parse_xml.py
+++ parse_xml.py
@@ -10,13 +10,27 @@ import os
 here = Path(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 
-template = here / 'mavlink-master/message_definitions/v1.0/ardupilotmega.xml'
+def load_model():
+    """
+    Example.
+    """
+    template = here / 'mavlink-master/message_definitions/v1.0/ardupilotmega.xml'
 
+    print('using file', template)
+    with open(template, 'r', encoding='utf-8') as file:
+        my_xml = file.read()
 
-print('using file', template)
-with open(template, 'r', encoding='utf-8') as file:
-    my_xml = file.read()
+    data_model = xmltodict.parse(my_xml)
 
-data_model = xmltodict.parse(my_xml)
+    dialect = data_model['mavlink']['enums']['enum']
+    child_models = dict()
 
-pprint.pprint(data_model, indent=2)
+    for enum in dialect:
+        name = enum['@name']
+        entry = enum['entry']
+
+        child_models[name] = entry
+
+    pprint.pprint(child_models, indent=2)
+
+    return child_models
