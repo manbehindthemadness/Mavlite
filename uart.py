877f5409c3db2f1c8063ee9fcfc905cdb8edd3dd
Kevin Eales <manbehindthemadness@gmail.com>
2022-08-17 18:00:21 -0400

    Example packet decoder.

--- uart.py
+++ uart.py
@@ -1,9 +1,4 @@
 # Write your code here :-)
-# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
-"""CircuitPython Essentials UART Serial example"""
 import board
 import busio
 import digitalio
@@ -13,38 +8,24 @@ uart = busio.UART(board.TX, board.RX, baudrate=115200)
 stream = list()
 packets = list()
 
-
-def find_with_list(myList, target):
-     inds = []
-     for i in range(len(myList)):
-         if myList[i] == target:
-             inds += i,
-     return inds
-
 while True:
     try:
         partial_buff = list()
         raw = uart.read(64)
-        # print(raw)
         data = list(raw)  # read up to 64 bytes
-
         if data is not None:
             beginning = False
             for idx, byte in enumerate(range(len(data))):
                 if data[byte] == 0xfd:  # Check for start condition.
-                    # print('found start', len(stream), len(partial_buff))
                     if stream:  # If we have a large partial stored from the last read iteration, finish it.
-                        # print('finishing large partial')
                         stream.extend(partial_buff)
                         packets.append(stream)
                     elif partial_buff:  # If the packets are smaller than the read buffer, handle them independently.
                         packets.append(partial_buff)
-                        # print('finishing small partial')
                     partial_buff = list()
                     stream = list()
                 partial_buff.append(data[byte])
             stream.extend(partial_buff)
-
             if len(packets):
                 for p in packets:
                     print('--------------------')
