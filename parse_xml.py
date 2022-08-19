f3e11a51f782d405f544310a9eb61a2fc74a4ff6
Kevin Eales <manbehindthemadness@gmail.com>
2022-08-19 09:27:49 -0400

    Revised XML parser to pull messages and commands into different lists.

---
+++ parse_xml.py
@@ -0,0 +1,33 @@
+"""
+This is an example to illustrate an idea.
+"""
+from pathlib import Path
+import xmltodict
+import os
+
+
+here = Path(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+
+
+def load_messages() -> (list, list):
+    """
+    Example.
+    """
+    template = here / 'message_definitions/v1.0/common.xml'
+
+    with open(template, 'r', encoding='utf-8') as file:
+        my_xml = file.read()
+
+    data_model = xmltodict.parse(my_xml)
+
+    messages = data_model['mavlink']['messages']['message']
+    enums = data_model['mavlink']['enums']['enum']
+    commands = dict()
+    for enum in enums:
+        if enum['@name'] == 'MAV_CMD':
+            commands = enum['entry']
+    return messages, commands
+
+
+
+
