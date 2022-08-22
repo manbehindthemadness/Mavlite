f7ae805a974ea5f5912a4149727aa1d1519681ee
Kevin Eales <manbehindthemadness@gmail.com>
2022-08-22 19:18:08 -0400

    Enhanced packet inspection to drop impossible values and reclaim the buffer memory.

--- src/uart.py
+++ src/uart.py
@@ -119,9 +119,6 @@ buffer_size = 8
 async def uart_read(_uart: any = None, callback: any = None, debug: bool = False) -> list:
     """
     Uart packet listener.
-
-    TODO: In the future we will need to constrain this traffic to only broadcast packets and packets that are
-            addressed to us. This could overflow real fast with more than a few devices on the network.
     """
     global stream
     global packets
@@ -153,43 +150,50 @@ async def uart_read(_uart: any = None, callback: any = None, debug: bool = False
                     skip -= 1
             stream.extend(partial_buff)
             if len(packets):
-                for p in packets:
+                for idx, p in enumerate(packets):
                     try:
-                        if debug:
-                            print('--------------------')
-                        try:
-                            message_id = struct.unpack("H", bytes(p[7:9]))[0]
-                        except (RuntimeError, ValueError) as err:
-                            print('Unable to unpack message_id', err, '\n', p[7:9])
-                            break
-                        pay_end = 10 + p[1]
-                        payload = p[10:pay_end]
-                        try:  # Prevent a failure from snowballing into logic down the line.
-                            crc = struct.unpack("H", bytes(p[pay_end:pay_end + 2]))[0]
-                        except RuntimeError as err:
-                            print(err, 'unable to decode crc\n', p, '\n', bytes(p))
-                            break  # Prevent a failure from snowballing into logic down the line.
-                        chk = p[1:pay_end]
+                        if 12 <= len(p) <= 280:  # 12 is the minimum MavLink packet length.
+                            if debug:
+                                print('--------------------')
+                            try:
+                                message_id = struct.unpack("H", bytes(p[7:9]))[0]
+                            except (RuntimeError, ValueError) as err:
+                                print('Unable to unpack message_id', err, '\n', p[7:9])
+                                del packets[idx]  # Clear memory.
+                                break
+                            pay_end = 10 + p[1]
+                            payload = p[10:pay_end]
+                            try:  # Prevent a failure from snowballing into logic down the line.
+                                crc = struct.unpack("H", bytes(p[pay_end:pay_end + 2]))[0]
+                            except RuntimeError as err:
+                                print(err, 'unable to decode crc\n', p, '\n', bytes(p))
+                                del packets[idx]  # Clear memory.
+                                break  # Prevent a failure from snowballing into logic down the line.
+                            chk = p[1:pay_end]
 
-                        msg = f'start {p[0]}, length {p[1]}, incompat {p[2]}, compat {p[3]}, seq {p[4]}, sys_id {p[5]}, '
-                        msg += f'comp_id {p[6]}, mes_id {message_id}, '
-                        msg += f'payload {payload}, crc {crc}, \nraw {bytes(p)}'
-                        pack = {
-                            'message_id': message_id,
-                            'system_id': p[5],
-                            'component_id': p[6],
-                            'payload': payload,
-                            'increment': p[4],
-                            'crc': crc,
-                            'chk': chk,
-                            'raw': bytes(p)
-                        }
-                        if callback:  # For CRC checking.
-                            if await callback(pack, debug):
-                                read_buffer.append(pack)
-                        read_buffer = read_buffer[-buffer_size:]
+                            msg = f'start {p[0]}, length {p[1]}, incompat {p[2]}, compat {p[3]}, seq {p[4]}, sys_id {p[5]}, '
+                            msg += f'comp_id {p[6]}, mes_id {message_id}, '
+                            msg += f'payload {payload}, crc {crc}, \nraw {bytes(p)}'
+                            pack = {
+                                'message_id': message_id,
+                                'system_id': p[5],
+                                'component_id': p[6],
+                                'payload': payload,
+                                'increment': p[4],
+                                'crc': crc,
+                                'chk': chk,
+                                'raw': bytes(p)
+                            }
+                            if callback:  # For CRC checking.
+                                if await callback(pack, debug):
+                                    read_buffer.append(pack)
+                            read_buffer = read_buffer[-buffer_size:]
+                        else:
+                            msg = f'dropping packet: {p}'
+                            del packets[idx]  # Clear memory.
                     except IndexError:
                         msg = f'bad_data {p}'
+                        del packets[idx]  # Clear memory.
                     if debug:
                         print('--------------------\n', 'receiving', msg)
                 packets = list()
